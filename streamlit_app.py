371787358b651e20960454b4dfeb8ed190d5a0fb
ootejreddy <40879507+ootejreddy@users.noreply.github.com>
2025-07-28 23:43:23 -0500

    Update streamlit_app.py
    Modified the for loop code to get the ingredient lists

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-    st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
     values('""" + ingredients_string +"""','"""+name_on_order+"""')
@@ -35,6 +36,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
         
