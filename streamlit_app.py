471e0e0377aae9fd7b0f40dc254f24614d6dc6b4
ootejreddy <40879507+ootejreddy@users.noreply.github.com>
2025-07-30 16:44:52 -0500

    Update streamlit_app.py
    added pandas and implemented dynamic api search parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,19 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_data_frame = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_data_frame = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_data_frame, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_data_frame,max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen+' Nutrition Information')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
